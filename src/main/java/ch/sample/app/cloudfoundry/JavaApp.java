88db9e4972b8dd135a72f826c590536011375344
Anton B <192003+nyfd@users.noreply.github.com>
2018-07-18 11:14:25 +0200

    fix: use the usual CF port (8080)

--- src/main/java/ch/sample/app/cloudfoundry/JavaApp.java
+++ src/main/java/ch/sample/app/cloudfoundry/JavaApp.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 public class JavaApp {
     public static void main(String[] args) {
+        port(8080);
         get("/", (req, res) -> "Your JavaApp is up and running!");
     }
 }
